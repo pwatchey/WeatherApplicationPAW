bfb528654f9e9ddc3b2110388a2627e24cb2eff0
pwatchey <paigewatchey@gmail.com>
2015-10-19 08:53:15 -0400

    inital commit

---
+++ javascript/javascript.js
@@ -0,0 +1,27 @@
+var d = new Date(); 
+var e = d.getMonth();
+
+ 
+if(e === 11 || e === 0 || e === 1) //december, january, february 
+{
+	//make the background picture a winter one 
+	
+}
+if else (e === 2 || e === 3 || e === 4)  //march, april, may 
+{
+	
+	//make the background picture a spring one 
+	
+}
+if else (e === 5 || e === 6 || e === 7) //june, july, august 
+{
+	
+	//make the background picture a summer one
+	
+}
+else //september, october, november
+{
+	
+	//make the background picture an autumn one. 
+	
+} 
